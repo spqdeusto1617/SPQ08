364a46d5e7dd5f1e968c6eb58112e7053ff4e71f
inigo <inigo.alon@opendeusto.es>
2017-04-04 20:43:35 +0200

    message to run the RMI on the main as comment

--- mailServer/src/main/java/Main.java
+++ mailServer/src/main/java/Main.java
@@ -5,6 +5,11 @@ import java.rmi.server.UnicastRemoteObject;
 /**
  * Created by inigo on 30/03/17.
  */
+
+//code to run on the terminal => rmiregistry -J-Djava.rmi.server.useCodebaseOnly=false
+//introduce JVM argument on Edit Configurations (in the future it is going to be in the pom.xml) + path Of project should be modified
+//      => -Djava.rmi.server.codebase=file:/home/inigo/workspace/Java/SPQ08/mailServer/target/classes/
+
 public class Main extends UnicastRemoteObject implements RMIInterface{
 
     protected Main() throws RemoteException {
