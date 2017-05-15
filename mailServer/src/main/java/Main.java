96196071d950a979c64d3d4958e20a67b71c579b
tzongo <gotzon.gerri@opendeusto.es>
2017-05-15 19:31:41 +0200

    Added new sign up

--- mailServer/src/main/java/Main.java
+++ mailServer/src/main/java/Main.java
@@ -6,16 +6,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        //Create user
-        MongoDB db = new MongoDB();
-        db.sign_up("inigo","inigo");
-        db.sign_up("gotzon","gotzon");
-        Email em3 = new Email("inigo2","gotzon","Hi","There");
-        try {
-            db.save_emails(em3);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        //Create user
+//        MongoDB db = new MongoDB();
+//        db.sign_up("inigo","inigo");
+//        db.sign_up("gotzon","gotzon");
+//        Email em3 = new Email("inigo2","gotzon","Hi","There");
+//        try {
+//            db.save_emails(em3);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         Server.runServer(args[0], args[1], args[2]);
 
     }
