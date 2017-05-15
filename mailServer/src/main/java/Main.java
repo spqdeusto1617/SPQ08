425b49c4eaa99a589c32da65bb3cdaebaa84cd3a
tzongo <gotzon.gerri@opendeusto.es>
2017-05-15 19:54:31 +0200

    sign up running

--- mailServer/src/main/java/Main.java
+++ mailServer/src/main/java/Main.java
@@ -7,15 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 //        //Create user
-//        MongoDB db = new MongoDB();
-//        db.sign_up("inigo","inigo");
-//        db.sign_up("gotzon","gotzon");
-//        Email em3 = new Email("inigo2","gotzon","Hi","There");
-//        try {
-//            db.save_emails(em3);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+       MongoDB db = new MongoDB();
+        db.sign_up("inigo","inigo",true);
         Server.runServer(args[0], args[1], args[2]);
 
     }
