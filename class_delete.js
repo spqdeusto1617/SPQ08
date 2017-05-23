7034862b641495d7017662dc249f79a677acf386
inigo <inigo.alon@opendeusto.es>
2017-05-23 11:38:23 +0200

    nope x2

---
+++ class_delete.js
@@ -0,0 +1,11 @@
+var class_delete =
+[
+    [ "Delete", "class_delete.html#acb6bc707d28650bdeb2283f87630e0b6", null ],
+    [ "getDate", "class_delete.html#a65c0b139726126a7c0a0bfef168bbe84", null ],
+    [ "getSource", "class_delete.html#acc45fccf90716ebcd88990f0c6c36d43", null ],
+    [ "getUser", "class_delete.html#a127124607f6d882f87a4deabc2a95e33", null ],
+    [ "setDate", "class_delete.html#ae1062b901bae5ee6d6722448a12af7aa", null ],
+    [ "setSource", "class_delete.html#a2bddae4762dd4231ab53ee8b2409130b", null ],
+    [ "setUser", "class_delete.html#a1154a1eb8b05320cf686691cd727dc50", null ],
+    [ "toString", "class_delete.html#ab39433411917f38404d915307d826600", null ]
+];
