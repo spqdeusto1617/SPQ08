e7357ec4cb2e1634f5c3454f2aefe1c286ee14e0
inigo <inigo.alon@opendeusto.es>
2017-05-23 11:22:59 +0200

    docs

---
+++ class_mongo_d_b.js
@@ -0,0 +1,18 @@
+var class_mongo_d_b =
+[
+    [ "MongoDB", "class_mongo_d_b.html#a7170cf8463b15fad9f4b8d9373b16201", null ],
+    [ "delete_message", "class_mongo_d_b.html#a1bc531a4e919dd942edeafc749e30f81", null ],
+    [ "getDatabaseName", "class_mongo_d_b.html#a5e60a5605d070637ce60a3dc4e67138a", null ],
+    [ "getDatabasePassword", "class_mongo_d_b.html#a86a8fe83d594dc26f709b9e048c79fe6", null ],
+    [ "getDb", "class_mongo_d_b.html#a70007164c403cb52ac8e87b0137aa27e", null ],
+    [ "getDbPasswordCollection", "class_mongo_d_b.html#aa46409219a8b26d1b0a229e464be7258", null ],
+    [ "getEmails", "class_mongo_d_b.html#afb31e6d36e9b20ceff3f13901bfc4012", null ],
+    [ "getUserCollection", "class_mongo_d_b.html#adf1dfa43d61be69c1673bb8ddfeb210e", null ],
+    [ "save_emails", "class_mongo_d_b.html#a2e376b333a71c82b5dd8e054d538e7f4", null ],
+    [ "setDb", "class_mongo_d_b.html#afa9b1ff69f975af22fdff7860c600c8e", null ],
+    [ "setDbPasswordCollection", "class_mongo_d_b.html#a54e2ada48adc6b47a40d5092113b2369", null ],
+    [ "sign_in", "class_mongo_d_b.html#a672df0039a1fcd302bd399089bb7fe28", null ],
+    [ "sign_in_as_root", "class_mongo_d_b.html#afa13d12f56548fcf6c5ca12ec66bc73c", null ],
+    [ "sign_up", "class_mongo_d_b.html#a9c69be7c091bffc9d1950e118fdb2251", null ],
+    [ "updatePassword", "class_mongo_d_b.html#a43b2b812eb5b88c87d2c3e2b100ae74b", null ]
+];
